dad5adbc466c713b5e7f5de8c2eeeb1af6e3be3d
maxbert <mbertfield@stuy.edu>
2016-12-15 14:49:36 -0500

    stuff

---
+++ intro.js
@@ -0,0 +1,26 @@
+
+
+var f = function(e) {
+    var list = document.getElementById("thelist");
+    var l = document.createElement("li");
+    l.innerHTML = "new thing!";
+    l.addEventListener('mouseover', d(l.innerHTML));
+    list.appendChild(l); 
+    
+};
+
+var bod = document.getElementById("b");
+console.log(bod)
+bod.addEventListener('click', f);
+    
+var listy = document.getElementsByTagName("li");
+var i = 0;
+var d = function(text) {
+    var head = document.getElementById("h");
+    head.innerHTML = text;
+};
+for(i; i < listy.length; i++){
+    listy[i].addEventListener('mouseover', d(listy[i].innetHTML));
+} 
+
+
